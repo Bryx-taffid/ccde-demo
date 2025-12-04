70cf249122c42070f51888558aead5c8530871c8
Bryx-taffid <175310370+Bryx-taffid@users.noreply.github.com>
2025-12-04 10:47:18 +0100

    chore: initial commit

---
+++ CCDE-Razor-App/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CCDE_Razor_App.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
