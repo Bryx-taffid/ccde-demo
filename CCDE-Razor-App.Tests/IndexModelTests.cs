0396c6e5493068e407f5e322aaef2fc2a687e4eb
Bryx-taffid <175310370+Bryx-taffid@users.noreply.github.com>
2025-12-11 09:40:36 +0100

    build: add unit test project
    * Creates the xUnit test project
    * Add Moq for mocking services
    * No direct code changes
    * No unit tests are written
    
    Refs: #8

---
+++ CCDE-Razor-App.Tests/IndexModelTests.cs
@@ -0,0 +1,11 @@
+﻿using Castle.Core.Configuration;
+using CCDE_Razor_App.Pages;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace CCDE_Razor_App.Tests;
+
+public class IndexModelTests
+{
+
+}
