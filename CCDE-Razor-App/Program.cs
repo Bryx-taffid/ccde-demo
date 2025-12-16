1e663bac48760b6d8b2b17560a3a736c87989fd9
Bryx-taffid <175310370+Bryx-taffid@users.noreply.github.com>
2025-12-16 22:29:51 +0100

    feat!: use Azure Key Vault and .NET secrets to remove credentials from .gitignored cleartext file
    BREAKING CHANGE: Configuration / set-up for Azure Key Vault and .NET user secrets as well as Azure Web App is needed.
    
    * Does not store cleartext credentials on the local machine anymore
    * Uses .NET secrets for development/local secrets
    * Uses Azure Key Vault for production
    * The Azure Key Vault URL is set in an environment variable in the Azure Web App
    
    Refs: #10

--- CCDE-Razor-App/Program.cs
+++ CCDE-Razor-App/Program.cs
@@ -1,4 +1,59 @@
-var builder = WebApplication.CreateBuilder(args);
+#if DEBUG
+using System.Text;
+#endif
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using CCDE_Razor_App;
+
+var environment =
+#if DEBUG
+    Environments.Development;
+#else
+    Environments.Production;
+#endif
+
+var keyName = GetSecret(SecretTypes.Key);
+var saltName = GetSecret(SecretTypes.Salt);
+var iterationName = GetSecret(SecretTypes.Iterations);
+
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    EnvironmentName = environment, // important: sets the correct environment (dev/prod) based on the run profile
+    ContentRootPath = AppContext.BaseDirectory,
+    WebRootPath = AppContext.BaseDirectory,
+    Args = args
+});
+
+if (builder.Environment.IsProduction())
+{
+    var keyVaultUrl = Environment.GetEnvironmentVariable("VAULT_URL");
+    if (string.IsNullOrWhiteSpace(keyVaultUrl))
+    {
+        throw new Exception("KeyVault URL is missing");
+    }
+
+    var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+    // get secrets (those are saved like key.value, salt.value,...)
+    var key = await client.GetSecretAsync(keyName);
+    var salt = await client.GetSecretAsync(saltName);
+    var iterationSecret = await client.GetSecretAsync(iterationName);
+    SecretHelper.Salt = salt.Value.Value;
+    SecretHelper.Key = key.Value.Value;
+    var result = int.TryParse(iterationSecret.Value.Value, out var iterations);
+    ValidateIntConversion(result, iterations);
+}
+else
+{
+    // get the secrets from .NET secrets
+    SecretHelper.Key = builder.Configuration[keyName];
+    SecretHelper.Salt = builder.Configuration[saltName];
+    var result = int.TryParse(builder.Configuration[iterationName], out var iterations);
+    ValidateIntConversion(result, iterations);
+}
+// TODO: get the secret for development from .NET user secrets
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -24,3 +79,41 @@ app.MapRazorPages()
     .WithStaticAssets();
 
 app.Run();
+return;
+
+// Converts string to int (if possible)
+void ValidateIntConversion(bool result, int iterations)
+{
+    if (result)
+        SecretHelper.Iterations = iterations;
+    else
+        throw new Exception("Iterations not found");
+}
+
+string GetSecret(SecretTypes secret)
+{
+    string secretName;
+    switch (secret)
+    {
+        case SecretTypes.Key:
+            secretName = "Key";
+            break;
+        case SecretTypes.Salt:
+            secretName = "Salt";
+            break;
+        case SecretTypes.Iterations:
+            secretName = "Iterations";
+            break;
+        default:
+            return "";
+    }
+
+#if DEBUG
+    var sb = new StringBuilder("Crypto:");
+    sb.Append(secretName);
+    var resultName = sb.ToString();
+#else
+    var resultName = secretName;
+#endif
+    return resultName;
+}
---
+++ CCDE-Razor-App/SecretHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace CCDE_Razor_App;
+
+/// <summary>
+/// Saves the secrets from Azure DevOps or .NET secrets
+/// </summary>
+internal static class SecretHelper
+{
+    internal static string? Key { get; set; }
+    internal static string? Salt { get; set; }
+    internal static int Iterations { get; set; }
+}
